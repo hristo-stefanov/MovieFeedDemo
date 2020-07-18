68d40213d7e10ed7d6389ab900f777c556026a36
Hristo M. Stefanov <hristo.m.stefanov@gmail.com>
2020-07-18 19:14:56 +0300

    fix service URL

--- app/src/main/java/hristostefanov/moviefeeddemo/Service.kt
+++ app/src/main/java/hristostefanov/moviefeeddemo/Service.kt
@@ -1,11 +1,11 @@
 package hristostefanov.moviefeeddemo
 
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.http.QueryName
-import rx.Single
 
 interface Service {
-    @GET("/movie/upcoming")
-    fun getMoveUpcoming(@Query("api_key") apiKey: String): Single<Response>
+    @GET("movie/upcoming")
+    fun getMoveUpcoming(@Query("api_key") apiKey: String, @Query("page") page: Int): Single<Response>
 }
